6de29653335999c1263a1da774c8b2091eb9fa44
bar tibarovsky <barooshgm@gmail.com>
2021-12-19 21:49:25 +0200

    initial commit

---
+++ ObjConstractor.js
@@ -0,0 +1,33 @@
+function BankAccount(owner, balance, lineOfCredit, loans, creditScore) {
+    this.owner = owner;
+    this.balance = balance;
+    this.lineOfCredit = lineOfCredit;
+    this.loans = loans;
+    this.creditScore = creditScore;
+    this.depositeMoney = function (sum) {
+        this.balance = balance + sum;
+    };
+    this.withdrawMoney = function (sum) {
+        this.balance = balance - sum;
+    };
+    this.increaseLineOfCredit = function (sum) {
+        this.lineOfCredit = lineOfCredit + sum;
+    };
+    this.takeLoan = function (sum) {
+        this.loans = loans + sum;
+    };
+    this.terminateLoan = function (sum) {
+        this.loans = loans - sum;
+    };
+    this.updateCreditScore = function (num) {
+        this.creditScore = creditScore + num;
+    };
+
+
+}
+
+const barBankAccount = new BankAccount("BarTibarovsky", 100000, 500000, 0, 100);
+barBankAccount.depositeMoney(200000);
+barBankAccount.increaseLineOfCredit(700000);
+barBankAccount.takeLoan(50000);
+barBankAccount.updateCreditScore(150);
